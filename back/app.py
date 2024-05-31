21986cce423483b74b87d5c4b161ca4a7c58d1aa
Antoine A <antoine_avenia@hotmail.fr>
2024-05-31 13:00:49 +0200

    add netlify cors

--- back/app.py
+++ back/app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from routes import register_routes
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://6659acc8497f3a1d9caaf899--hackathon-jo-19.netlify.app"}})
 
 register_routes(app)
 
